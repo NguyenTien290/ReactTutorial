a4a30fc831c33f40c32e3a454e95362ca52799be
NguyenTien <kythuat09.masterpro@gmail.com>
2024-10-10 17:15:33 +0700

    FilterAndSearch

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Minnigame from './Minnigame';
+import List from './List'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <Minnigame />
+   <List />
   </React.StrictMode>
 );
 
